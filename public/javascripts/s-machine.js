d2cda98af7c54e77136d5f99085c92a10ce1482e
levi <cannabishemplanet@gmail.com>
2018-10-23 16:59:59 -0700

    Init Changes

---
+++ public/javascripts/s-machine.js
@@ -0,0 +1,15 @@
+// --- reserved for tooltips JQuery JS function
+// Function for Toggling Panels
+function togglePanel(panelId, buttonId) {
+    var p = document.getElementById(panelId);
+    var pbutton = document.getElementById(buttonId);
+    if (p.classList.contains('visible')) {
+        p.classList.replace('visible', 'invisible');
+        pbutton.classList.replace('badge-success', 'badge-danger');
+        console.log('toggled panel: ' + p);
+    } else {
+        p.classList.replace('invisible', 'visible');
+        pbutton.classList.replace('badge-danger', 'badge-success');
+        console.log('toggled pane0: ' + p);
+    }
+}
